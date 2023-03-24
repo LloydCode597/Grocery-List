4af59c81ad15fb34eaad0020a0e95b05164ad58b
Lloyd Allen <110001021+lloydallen597@users.noreply.github.com>
2023-03-23 23:21:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
+
+// delete items using the clear item function
+clearBtn.addEventListener("click", clearItems);
+
+// ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+
+    // add class to element
+    element.classList.add("grocery-item");
+
+    // add id to element
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div> `;
+    const deleteBtn = element.querySelector(".delete-btn");
+    const editBtn = element.querySelector(".edit-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+
+    // append child
+    list.appendChild(element);
+
+    // display alert
+    displayAlert("Item added to the list,", "successful");
+
+    // show container
+    container.classList.add("show-container");
+
+    // add to local storage
+    addToLocalStorage(id, value);
+
+    // set back to default
+    setBackToDefault();
+  } else if (value && editFlag) {
+    console.log("editing item to the list");
+  } else {
+    displayAlert("please enter value", "danger");
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+//clear items from
+function clearItems() {
+  const item = document.querySelectorAll(".grocery-item");
+
+  if (item.length > 0) {
+    item.forEach(function (item) {
+      list.removeChild(item); // remove the item from
+    });
+  }
+
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger"); // remove the list from
+  setBackToDefault(); // reset back to default state after resetting
+  // localStorage.removeItem("list");
+}
+
+function deleteItem() {
+  console.log("Item Deleted");
+}
+
+function editItem() {
+  console.log("Edit Item");
+}
+
+// set back to default
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "submit";
+}
+
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+  console.log("adding to local storage");
+}
+// ****** SETUP ITEMS **********
